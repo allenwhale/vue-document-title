dc54f037bd5b3ade465d55a59b1588ff352c2945
zhenyong <116982086@qq.com>
2016-08-22 23:48:39 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # vue-document-title
-Vue.js version of [react-document-title](https://github.com/gaearon/react-document-title)
+
+> Vue.js version of [react-document-title](https://github.com/gaearon/react-document-title)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+## Usage
+
+```vue
+<template>
+<document-title :title="caption">...sub...</document-title>
+</template>
+
+<script>
+import DocumentTitle from 'vue-document-title'
+
+const data = {caption: 'first title'}
+export default {
+  data: function () {
+    return data
+  }
+}
+
+setTimeout(function () {
+  data.caption = 'next title'
+}, 2000)
+</script>
+```
+
+
+
+
+
+## TODO
+
+- [ ] test
+- [ ] flowtype
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var path = require('path')
+var webpack = require('webpack')
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  // target: 'node',
+  // externals: [nodeExternals()],
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'index.js',
+    libraryTarget: "umd",
+    library: 'VueDocumentTitle'
+  },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file',
+        query: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ])
+}
